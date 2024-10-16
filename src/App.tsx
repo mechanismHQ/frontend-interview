e6333ecbb2b205ec02952af90ee9f30bc9454470
cipherZ <dev@cipherz.com>
2024-10-16 09:49:08 -0400

    removed apollo refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import apolloLogo from "./assets/apollo-logo.jpeg";
 import styles from "./App.module.css";
-import classnames from "classnames";
 
 function App() {
   return (
     <div className={styles.root}>
-      <a href="https://www.apollo.io/tech-blog" target="_blank">
-        <img
-          src={apolloLogo}
-          className={classnames(styles.logo)}
-          alt="Apollo logo"
-        />
-      </a>
-      <h1>Thanks for interviewing with Apollo!</h1>
+      <h1>Thanks for interviewing with Mechanism!</h1>
       &#8249;Your code here&#8250;
     </div>
   );
